cdc9955dabcc6da8d28eb3958c74ed3fff5c5d6e
jshahriyarbadalov <shah.badalov91@gmail.com>
2019-10-10 23:03:57 -0700

    Navigation Bar Lesson
    Navigation bar practrice lesson note app

---
+++ SwiftBeginner-L1/SwiftBeginner-L1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftBeginner-L1
+//
+//  Created by Shahriyar Badalov on 10/9/19.
+//  Copyright © 2019 Shahriyar Badalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
